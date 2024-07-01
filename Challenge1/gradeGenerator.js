1a2568e2a967851d47f3a3b571a2b3f92b35937f
abdirahmanmuktar <abdirahmanmuktar18@gmail.com>
2024-07-01 18:26:35 +0700

    i createdSpeed.js file

---
+++ Challenge1/gradeGenerator.js
@@ -0,0 +1,23 @@
+// Function to generate student grade based on input marks
+function generateGrade(marks) {
+    
+    let grade;
+
+    if (marks > 79){ 
+    grade = "A";
+    } else if (marks >= 60 ) {
+    grade = "B";
+
+   } else if (marks >= 50){
+    grade = "C";
+
+   }else if (marks >= 40) {
+    grade = "D";
+
+   } else {
+    grade = "E";
+
+  }
+ 
+return grade;
+ }
